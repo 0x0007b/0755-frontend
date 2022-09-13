46c9cf20ff391c81611c4314fe890e016efe8968
0x0007b <91422256+0x0007b@users.noreply.github.com>
2022-09-13 21:20:29 +0300

    Update Itproger

--- Yahoobot.js
+++ Yahoobot.js
@@ -1,36 +1,75 @@
 // ==UserScript==
-// @name         YahooBot
+// @name         BingBot
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
+// @version      1.0
+// @description  Bot for Bing
 // @author       Vladislav Skrynnikov
-// @match        https://www.yahoo.com/*
-// @icon         https://w7.pngwing.com/pngs/351/399/png-transparent-logo-computer-icons-social-network-yahoo-search-email-miscellaneous-purple-violet.png
+// @match        https://www.bing.com/*
+// @match        https://tproger.ru/*
+// @icon         https://pushall.ru/upimg/sub641-1458837242.png
 // @grant        none
 // ==/UserScript==
 
-let links = document.links;
-let btnK = document.getElementById("ybar-search")
-let keywords = ["вывод произвольных полей wordpress", "10 самых популярных шрифтов от Google", "Отключение редакций и ревизий в WordPress"];
-let keyword = keywords[getRandom(0, keywords.length)]
+let links = document.links
+let search = document.getElementsByName("search")[0];
+let keywords = ["как стать разработчиком?", "Как стать программистом с нуля в 2022", "как стать программистом, имея самые базовые представления о разработке в целом"];
+let keyword = keywords[getRandom(0, keywords.length)];
+let BingInput = document.getElementsByName("q")[0];
+let sb_bp = document.getElementsByClassName("sb_bp")[5]
 
-document.getElementById("ybar-sbq").value = keyword;
-document.getElementById("ybar-search").click();
+if (search !== undefined) {
+  let i = 0;
 
-if (btnK !== undefined) {
-    btnK.click;
+  let timerId = setInterval(function() {
+    BingInput.value += keyword[i];
+    i++;
+    if (i == keyword.length) {
+      clearInterval(timerId);
+      setTimeout(()=>{
+        search.click();
+      }, getRandom(1000, 2500));
+
+    }
+  }, 300);
+
+
+}else if (location.hostname == "tproger.ru") {
+  console.log("Мы на целевом сайте");
+  setInterval(()=>{
+    let index = getRandom(0, links.length);
+    if (getRandom(0, 101) > 70) {
+      location.href = "https://www.bing.com/";
+    }
+    if (links[index].href.indexOf("tproger.ru") !== -1) {
+      links[index].click();
+    }
+
+  }, getRandom(1000, 2500));
+
+
 } else {
-
-for (let i = 0; i < links.length; i++) {
-    if (links[i].href.indexOf("napli.ru") !== -1) {
-        console.log("Нашел строку" + links[i]);
-        let link = links[i];
+  let nextBingPage = true;
+  for (let i=0; i<links.length; i++) {
+    if (links[i].href.indexOf("tproger.ru") !== -1) {
+      console.log("Нашел строку " + links[i]);
+      let link = links[i];
+      nextBingPage = false;
+      setTimeout(()=>{
         link.click();
-        break;
+      }, getRandom(1000, 2500))
+      break;
+    }
+  }
+  if (document.querySelector(".sb_pagS").innerText == "4") {
+    nextBingPage = false;
+    location.href = "https://www.bing.com/";
+  }
+  if (nextBingPage) {
+    setTimeout(()=>{
+      sb_bp.click()
+    }, getRandom(1000, 2500));
+  }
 }
-}
-}
-
 function getRandom(min, max) {
-return Math.floor(Math.random()*(max - min) + min)
+  return Math.floor(Math.random()*(max - min) + min);
 }
